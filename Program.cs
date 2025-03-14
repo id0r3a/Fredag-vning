2956fa21fc401bc9b8c25676d8c55c3c2aa44f27
Dorsa Moradi <dorsamoradi2016@gmail.com>
2025-03-14 10:36:34 +0100

    Anagramgruppering

--- Program.cs
+++ Program.cs
@@ -1,10 +1,44 @@
 ﻿namespace FredagÖvning
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static List<List<string>> GrupperaAnagram(List<string> ordLista)
         {
-            Console.WriteLine("Hello, World!");
+            // Dictionary för att gruppera anagram
+            Dictionary<string, List<string>> anagramDictionary = new Dictionary<string, List<string>>();
+
+            // Iterera genom orden
+            foreach (string ord in ordLista)
+            {
+                // Sortera bokstäverna i ordet och skapa en unik nyckel utan String.Concat()
+                string sorteratOrd = string.Join("", ord.OrderBy(tecken => tecken));
+
+                // Lägg till i Dictionaryn
+                if (anagramDictionary.ContainsKey(sorteratOrd))
+                {
+                    anagramDictionary[sorteratOrd].Add(ord);
+                }
+                else
+                {
+                    anagramDictionary[sorteratOrd] = new List<string> { ord };
+                }
+            }
+
+            // Konvertera Dictionaryn till en lista med grupper
+            return anagramDictionary.Values.ToList();
+        }
+
+        // Testfall
+        public static void Main(string[] args)
+        {
+            var input = new List<string> { "listen", "silent", "enlist", "rat", "tar", "art", "evil", "vile", "live" };
+            var resultat = GrupperaAnagram(input);
+
+            // Skriv ut resultatet
+            foreach (var grupp in resultat)
+            {
+                Console.WriteLine(string.Join(", ", grupp));
+            }
         }
     }
 }
